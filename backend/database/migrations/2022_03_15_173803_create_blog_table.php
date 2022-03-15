f9f32195baece1953a21099a13a3c0bad7bb371b
HenryGye <henryedua@hotmail.com>
2022-03-15 13:01:17 -0500

    Creacion de migraciones tablas usuario y blog

---
+++ backend/database/migrations/2022_03_15_173803_create_blog_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->string('titulo', 100);
+            $table->string('descripcion', 255);
+            $table->dateTime('fecha_publicacion', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
