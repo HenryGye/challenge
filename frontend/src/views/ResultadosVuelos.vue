abac95ea67a31e6d4ab1b56648eed6598002b851
HenryGye <henryedua@hotmail.com>
2022-03-16 15:56:37 -0500

    crea vista Resultado de vuelos e integrar componente buscador y componente de resultados

---
+++ frontend/src/views/ResultadosVuelos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <BuscadorVuelos/>
+    <Resultados/>
+  </div>
+</template>
+
+<script>
+  import BuscadorVuelos from '../components/BuscadorVuelos.vue';
+  import Resultados from '../components/Resultados.vue';
+  export default {
+    components: {
+      BuscadorVuelos,
+      Resultados,
+    },
+    created() {
+      if (!this.$store.state.busqueda) this.$router.push({ name: "vuelos" });
+    }
+  };
+</script>
