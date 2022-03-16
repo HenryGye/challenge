9f88fb2345c44a19762ef5f0602e98d0a5f82a82
HenryGye <henryedua@hotmail.com>
2022-03-15 19:12:05 -0500

    Mapeo rutas api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UsuarioController;
+use App\Http\Controllers\API\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(UsuarioController::class)->group(function () {
+    Route::get('/usuario', 'index');
+    Route::get('/usuario/{id}', 'show')->where('id', '[0-9]+');
+});
+
+Route::controller(BlogController::class)->group(function () {
+    Route::get('/blog', 'index');
+    Route::get('/blog/{id}', 'show')->where('id', '[0-9]+');
+});
+
+Route::fallback(function(){
+    return response()->json(['message' => 'Ruta no encontrada']);
 });
