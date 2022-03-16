2103d33edb93528a6d969e18c610bfa9b1780014
HenryGye <henryedua@hotmail.com>
2022-03-16 15:52:04 -0500

    Agregar componente Navbar al App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <b-container fluid>
+      <NavBar/>
+      <router-view class="text-center"/>
+    </b-container>
   </div>
 </template>
 
+<script>
+  import NavBar from './components/NavBar.vue';
+  export default {
+    components: {
+      NavBar,
+    },
+  };
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container-fluid {
+  padding: 0 !important;
+}
+
+#nav-collapse a.active-routes {
+  color: white;
 }
 </style>
