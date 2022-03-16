3d83e239d4d2730fd59524b622b4137a9c92601f
HenryGye <henryedua@hotmail.com>
2022-03-16 15:50:34 -0500

    Ajustar archivos de rutas

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,17 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Vuelos from './views/Vuelos.vue'
+import ResultadosVuelos from './views/ResultadosVuelos.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   // base: process.env.BASE_URL,
+  linkExactActiveClass: "active-routes",
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/vuelos',
+      name: 'vuelos',
+      component: Vuelos
+    },
+    {
+      path: '/resultados-vuelos',
+      name: 'resultadosVuelos',
+      component: ResultadosVuelos
     },
   ]
 })
