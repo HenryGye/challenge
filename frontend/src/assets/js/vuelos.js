45c60eca60ec7880664d00b7f07a6ef749848e94
HenryGye <henryedua@hotmail.com>
2022-03-16 15:57:23 -0500

    Crear archivo vuelos.js con data random

---
+++ frontend/src/assets/js/vuelos.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    id: 1,
+    duracion: {
+      hora_salida: '18:24',
+      hora_llegada: '21:06',
+      dias: '+1d 24h 42min'
+    },
+    escala: '1',
+    aerolinea: 'CopaAirlines',
+    precio: '868'
+  },
+  {
+    id: 2,
+    duracion: {
+      hora_salida: '17:15',
+      hora_llegada: '09:19',
+      dias: '+1d 18h 04min'
+    },
+    escala: '1',
+    aerolinea: 'Avianca',
+    precio: '962'
+  },
+  {
+    id: 3,
+    duracion: {
+      hora_salida: '20:10',
+      hora_llegada: '21:45',
+      dias: '+1d 19h 35min'
+    },
+    escala: '1',
+    aerolinea: 'KLM',
+    precio: '1192'
+  },
+  {
+    id: 4,
+    duracion: {
+      hora_salida: '23:59',
+      hora_llegada: '00:58',
+      dias: '+2d 26h 59min'
+    },
+    escala: '2',
+    aerolinea: 'Spirit',
+    precio: '1000'
+  },
+  {
+    id: 5,
+    duracion: {
+      hora_salida: '22:20',
+      hora_llegada: '22:41',
+      dias: '+1d 22h 21min'
+    },
+    escala: '2',
+    aerolinea: 'DELTA',
+    precio: '1823'
+  },
+  {
+    id: 6,
+    duracion: {
+      hora_salida: '19:50',
+      hora_llegada: '11:55',
+      dias: '+1d 9h 18h 05min'
+    },
+    escala: '1',
+    aerolinea: 'LATAM',
+    precio: '1870'
+  },
+]
